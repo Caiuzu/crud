a632edd2010cf7a22c9485a6b956525356ab268f
Caio Souza <caio.7s@outlook.com>
2022-06-06 18:17:18 -0300

    feat: Criando entidade Student

---
+++ src/main/java/br/com/simple/crud/domain/entity/Student.java
@@ -0,0 +1,67 @@
+package br.com.simple.crud.domain.entity;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.Hibernate;
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Student {
+    @Id
+    @Column(nullable = false)
+    @Setter(AccessLevel.NONE)
+    @GeneratedValue
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(nullable = false)
+    @NotEmpty(message = "Nome não informado")
+    @Length(max = 10, message = "Nome muito extenso")
+    private String name;
+
+    @Column(nullable = false)
+    @NotEmpty(message = "Sobrenome não informado")
+    @Length(max = 10, message = "Sobrenome muito extenso")
+    private String lastName;
+
+    @Column(nullable = false)
+    @NotNull(message = "Idade não informada")
+    private Integer age;
+
+    public Student(String name, String lastName, Integer age) {
+        this.name = name;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Student student = (Student) o;
+        return id != null && Objects.equals(id, student.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
