82a44aae72cef9c22b9b755e338f0ba129e1e1f4
Caio Souza <caio.7s@outlook.com>
2022-10-25 01:18:24 -0300

    style: Ajustando imports

--- src/main/java/br/com/simple/crud/resource/student/StudentResource.java
+++ src/main/java/br/com/simple/crud/resource/student/StudentResource.java
@@ -6,6 +6,7 @@ import br.com.simple.crud.domain.entity.Student;
 import br.com.simple.crud.service.StudentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
