0cc108dbbb9a578667162260fac1ffd00bed13fa
Caio Souza <caio.7s@outlook.com>
2022-12-11 15:20:25 -0300

    test: Adicionando execução de cucumber em porta randomica

--- src/test/java/br/com/simple/crud/cucumber/CucumberTest.java
+++ src/test/java/br/com/simple/crud/cucumber/CucumberTest.java
@@ -1,13 +1,15 @@
 package br.com.simple.crud.cucumber;
 
+import br.com.simple.crud.CrudApplication;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.junit.runner.RunWith;
-
+import org.springframework.boot.test.context.SpringBootTest;
 
 @RunWith(Cucumber.class)
 @CucumberContextConfiguration()
 @CucumberOptions(features = "src/test/resources/features", glue = "br.com.simple.crud.cucumber.step")
+@SpringBootTest(classes = {CrudApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CucumberTest {
 }
