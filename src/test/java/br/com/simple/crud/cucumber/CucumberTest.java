2ef6d0d59cf46eb338bb56454c16fef1de7639b3
Caio Souza <caio.7s@outlook.com>
2022-12-27 00:37:57 -0300

    test: Refatorando configuração de cucumber no projeto

--- src/test/java/br/com/simple/crud/cucumber/CucumberTest.java
+++ src/test/java/br/com/simple/crud/cucumber/CucumberTest.java
@@ -1,6 +1,5 @@
 package br.com.simple.crud.cucumber;
 
-import br.com.simple.crud.CrudApplication;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import io.cucumber.spring.CucumberContextConfiguration;
@@ -9,7 +8,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @RunWith(Cucumber.class)
 @CucumberContextConfiguration()
-@CucumberOptions(features = "src/test/resources/features", glue = "br.com.simple.crud.cucumber.step")
-@SpringBootTest(classes = {CrudApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@CucumberOptions(plugin = "pretty",
+        glue = "br.com.simple.crud.cucumber",
+        features = "src/test/resources/features")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CucumberTest {
 }
